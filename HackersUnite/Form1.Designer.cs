9f6ade73cc4292ae0d6ac2c0be4814d69ee53463
hammerzaine <hammerzaine@gmail.com>
2015-06-14 23:46:05 -0500

    Got more stuff working
    Got the Create Agent screen working.
    Got the Create Agent button working.
    Got the Cancel button in the Create Agent screen working.
    Known Bug:
    Clicking on Create Agent will lock the game up which will force you to
    do a hard close of the game. It is somewhere in the Save portion of the
    coding.

--- HackersUnite/Form1.Designer.cs
+++ HackersUnite/Form1.Designer.cs
@@ -40,7 +40,22 @@
             this.agentlist = new System.Windows.Forms.GroupBox();
             this.newagentbutton = new System.Windows.Forms.Button();
             this.newagentlabel = new System.Windows.Forms.Label();
+            this.agentlistlabel = new System.Windows.Forms.Label();
+            this.createagentbox = new System.Windows.Forms.GroupBox();
+            this.createagentlabel = new System.Windows.Forms.Label();
+            this.newagentcancelbutton = new System.Windows.Forms.Button();
+            this.newagentcancellabel = new System.Windows.Forms.Label();
+            this.createusernamelabel = new System.Windows.Forms.Label();
+            this.createpasswordlabel = new System.Windows.Forms.Label();
+            this.createconfirmpasswordlabel = new System.Windows.Forms.Label();
+            this.createusername = new System.Windows.Forms.TextBox();
+            this.createpassword = new System.Windows.Forms.TextBox();
+            this.createconfirmpassword = new System.Windows.Forms.TextBox();
+            this.createagentbutton = new System.Windows.Forms.Button();
+            this.createagenterror = new System.Windows.Forms.Label();
             this.agentloginbox.SuspendLayout();
+            this.agentlist.SuspendLayout();
+            this.createagentbox.SuspendLayout();
             this.SuspendLayout();
             // 
             // exitbutton
@@ -85,7 +100,7 @@
             this.agentloginboxlabel.AutoSize = true;
             this.agentloginboxlabel.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.agentloginboxlabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.agentloginboxlabel.Location = new System.Drawing.Point(88, 16);
+            this.agentloginboxlabel.Location = new System.Drawing.Point(76, 0);
             this.agentloginboxlabel.Name = "agentloginboxlabel";
             this.agentloginboxlabel.Size = new System.Drawing.Size(191, 35);
             this.agentloginboxlabel.TabIndex = 0;
@@ -139,6 +154,7 @@
             // 
             // agentlist
             // 
+            this.agentlist.Controls.Add(this.agentlistlabel);
             this.agentlist.Location = new System.Drawing.Point(12, 40);
             this.agentlist.Name = "agentlist";
             this.agentlist.Size = new System.Drawing.Size(200, 300);
@@ -152,6 +168,7 @@
             this.newagentbutton.Size = new System.Drawing.Size(35, 35);
             this.newagentbutton.TabIndex = 4;
             this.newagentbutton.UseVisualStyleBackColor = true;
+            this.newagentbutton.Click += new System.EventHandler(this.newagentbutton_Click);
             // 
             // newagentlabel
             // 
@@ -165,12 +182,153 @@
             this.newagentlabel.Text = "New Agent";
             this.newagentlabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // agentlistlabel
+            // 
+            this.agentlistlabel.AutoSize = true;
+            this.agentlistlabel.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.agentlistlabel.ForeColor = System.Drawing.Color.White;
+            this.agentlistlabel.Location = new System.Drawing.Point(74, -12);
+            this.agentlistlabel.Name = "agentlistlabel";
+            this.agentlistlabel.Size = new System.Drawing.Size(62, 35);
+            this.agentlistlabel.TabIndex = 6;
+            this.agentlistlabel.Text = "Agents";
+            this.agentlistlabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // createagentbox
+            // 
+            this.createagentbox.BackColor = System.Drawing.Color.Gray;
+            this.createagentbox.Controls.Add(this.createagenterror);
+            this.createagentbox.Controls.Add(this.createagentbutton);
+            this.createagentbox.Controls.Add(this.createconfirmpassword);
+            this.createagentbox.Controls.Add(this.createpassword);
+            this.createagentbox.Controls.Add(this.createusername);
+            this.createagentbox.Controls.Add(this.createconfirmpasswordlabel);
+            this.createagentbox.Controls.Add(this.createpasswordlabel);
+            this.createagentbox.Controls.Add(this.createusernamelabel);
+            this.createagentbox.Controls.Add(this.createagentlabel);
+            this.createagentbox.Location = new System.Drawing.Point(642, 34);
+            this.createagentbox.Name = "createagentbox";
+            this.createagentbox.Size = new System.Drawing.Size(350, 150);
+            this.createagentbox.TabIndex = 6;
+            this.createagentbox.TabStop = false;
+            // 
+            // createagentlabel
+            // 
+            this.createagentlabel.AutoSize = true;
+            this.createagentlabel.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createagentlabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.createagentlabel.Location = new System.Drawing.Point(117, 0);
+            this.createagentlabel.Name = "createagentlabel";
+            this.createagentlabel.Size = new System.Drawing.Size(104, 35);
+            this.createagentlabel.TabIndex = 0;
+            this.createagentlabel.Text = "Create Agent";
+            // 
+            // newagentcancelbutton
+            // 
+            this.newagentcancelbutton.Location = new System.Drawing.Point(923, 201);
+            this.newagentcancelbutton.Name = "newagentcancelbutton";
+            this.newagentcancelbutton.Size = new System.Drawing.Size(35, 35);
+            this.newagentcancelbutton.TabIndex = 7;
+            this.newagentcancelbutton.UseVisualStyleBackColor = true;
+            this.newagentcancelbutton.Click += new System.EventHandler(this.newagentcancelbutton_Click);
+            // 
+            // newagentcancellabel
+            // 
+            this.newagentcancellabel.AutoSize = true;
+            this.newagentcancellabel.Font = new System.Drawing.Font("Gabriola", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newagentcancellabel.ForeColor = System.Drawing.Color.White;
+            this.newagentcancellabel.Location = new System.Drawing.Point(783, 177);
+            this.newagentcancellabel.Name = "newagentcancellabel";
+            this.newagentcancellabel.Size = new System.Drawing.Size(92, 59);
+            this.newagentcancellabel.TabIndex = 8;
+            this.newagentcancellabel.Text = "Cancel";
+            this.newagentcancellabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // createusernamelabel
+            // 
+            this.createusernamelabel.AutoSize = true;
+            this.createusernamelabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createusernamelabel.Location = new System.Drawing.Point(11, 31);
+            this.createusernamelabel.Name = "createusernamelabel";
+            this.createusernamelabel.Size = new System.Drawing.Size(70, 19);
+            this.createusernamelabel.TabIndex = 6;
+            this.createusernamelabel.Text = "Username";
+            // 
+            // createpasswordlabel
+            // 
+            this.createpasswordlabel.AutoSize = true;
+            this.createpasswordlabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createpasswordlabel.Location = new System.Drawing.Point(12, 60);
+            this.createpasswordlabel.Name = "createpasswordlabel";
+            this.createpasswordlabel.Size = new System.Drawing.Size(69, 19);
+            this.createpasswordlabel.TabIndex = 6;
+            this.createpasswordlabel.Text = "Password";
+            // 
+            // createconfirmpasswordlabel
+            // 
+            this.createconfirmpasswordlabel.AutoSize = true;
+            this.createconfirmpasswordlabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createconfirmpasswordlabel.Location = new System.Drawing.Point(12, 89);
+            this.createconfirmpasswordlabel.Name = "createconfirmpasswordlabel";
+            this.createconfirmpasswordlabel.Size = new System.Drawing.Size(122, 19);
+            this.createconfirmpasswordlabel.TabIndex = 7;
+            this.createconfirmpasswordlabel.Text = "Confirm Password";
+            // 
+            // createusername
+            // 
+            this.createusername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.createusername.Location = new System.Drawing.Point(140, 35);
+            this.createusername.Name = "createusername";
+            this.createusername.Size = new System.Drawing.Size(150, 13);
+            this.createusername.TabIndex = 6;
+            this.createusername.TextChanged += new System.EventHandler(this.createusername_TextChanged);
+            // 
+            // createpassword
+            // 
+            this.createpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.createpassword.Location = new System.Drawing.Point(140, 64);
+            this.createpassword.Name = "createpassword";
+            this.createpassword.PasswordChar = '*';
+            this.createpassword.Size = new System.Drawing.Size(150, 13);
+            this.createpassword.TabIndex = 8;
+            // 
+            // createconfirmpassword
+            // 
+            this.createconfirmpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.createconfirmpassword.Location = new System.Drawing.Point(140, 93);
+            this.createconfirmpassword.Name = "createconfirmpassword";
+            this.createconfirmpassword.PasswordChar = '*';
+            this.createconfirmpassword.Size = new System.Drawing.Size(150, 13);
+            this.createconfirmpassword.TabIndex = 9;
+            // 
+            // createagentbutton
+            // 
+            this.createagentbutton.Location = new System.Drawing.Point(264, 121);
+            this.createagentbutton.Name = "createagentbutton";
+            this.createagentbutton.Size = new System.Drawing.Size(80, 23);
+            this.createagentbutton.TabIndex = 6;
+            this.createagentbutton.Text = "Create Agent";
+            this.createagentbutton.UseVisualStyleBackColor = true;
+            this.createagentbutton.Click += new System.EventHandler(this.createagentbutton_Click);
+            // 
+            // createagenterror
+            // 
+            this.createagenterror.AutoSize = true;
+            this.createagenterror.ForeColor = System.Drawing.Color.Red;
+            this.createagenterror.Location = new System.Drawing.Point(12, 126);
+            this.createagenterror.Name = "createagenterror";
+            this.createagenterror.Size = new System.Drawing.Size(0, 13);
+            this.createagenterror.TabIndex = 10;
+            // 
             // HackersUnite
             // 
             this.AccessibleName = "HackersUnite";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1004, 352);
+            this.Controls.Add(this.newagentcancellabel);
+            this.Controls.Add(this.newagentcancelbutton);
+            this.Controls.Add(this.createagentbox);
             this.Controls.Add(this.newagentlabel);
             this.Controls.Add(this.newagentbutton);
             this.Controls.Add(this.agentlist);
@@ -182,6 +340,10 @@
             this.Load += new System.EventHandler(this.HackersUnite_Load);
             this.agentloginbox.ResumeLayout(false);
             this.agentloginbox.PerformLayout();
+            this.agentlist.ResumeLayout(false);
+            this.agentlist.PerformLayout();
+            this.createagentbox.ResumeLayout(false);
+            this.createagentbox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,6 +363,19 @@
         private System.Windows.Forms.GroupBox agentlist;
         private System.Windows.Forms.Button newagentbutton;
         private System.Windows.Forms.Label newagentlabel;
+        private System.Windows.Forms.Label agentlistlabel;
+        private System.Windows.Forms.GroupBox createagentbox;
+        private System.Windows.Forms.Label createagentlabel;
+        private System.Windows.Forms.Button newagentcancelbutton;
+        private System.Windows.Forms.Label newagentcancellabel;
+        private System.Windows.Forms.Button createagentbutton;
+        private System.Windows.Forms.TextBox createconfirmpassword;
+        private System.Windows.Forms.TextBox createpassword;
+        private System.Windows.Forms.TextBox createusername;
+        private System.Windows.Forms.Label createconfirmpasswordlabel;
+        private System.Windows.Forms.Label createpasswordlabel;
+        private System.Windows.Forms.Label createusernamelabel;
+        private System.Windows.Forms.Label createagenterror;
     }
 }
 
--- HackersUnite/Form1.cs
+++ HackersUnite/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace HackersUnite
 {
@@ -27,17 +29,69 @@ namespace HackersUnite
             this.WindowState = FormWindowState.Maximized;
 
             this.BackColor = ColorTranslator.FromHtml("#181818"); //Sets the background to Charcoal
-            exitbutton.Location = new Point(1100, 500); //Sets exit button location
-            exitbuttonlabel.Location = new Point(1030, 487); //Sets exit button label location
+            exitbutton.Location = new Point(1100, 500); //Sets Exit Button location
+            exitbuttonlabel.Location = new Point(1030, 487); //Sets Exit Button label location
             agentloginbox.Location = new Point(500, 300); //Sets the Agent login Box location
             agentlist.Location = new Point(100, 230); //Sets the Agent List Box location
             newagentbutton.Location = new Point(1100, 450); //Sets the New Agent Button location
             newagentlabel.Location = new Point(960, 437); //Sets the New Agent Label location
+            createagentbox.Visible = false; //Sets the Create Agent Box invisable
+            newagentcancellabel.Visible = false; //Sets the New Agent Cancel Label invisable
+            newagentcancelbutton.Visible = false; //Sets the New Agent Cancel button invisable
+            newagentcancelbutton.Location = new Point(1100, 450); //Sets the New Agent Cancel button location
+            newagentcancellabel.Location = new Point(1000, 437); //Sets the New Agent Cancel label location
+            createagentbox.Location = new Point(500, 300); //Sets the Create Agent Box location
         }
 
         private void exitbutton_Click(object sender, EventArgs e)
         {
             Close(); //Closes the Window
+        }
+
+        private void newagentbutton_Click(object sender, EventArgs e)
+        {
+            agentlist.Visible = false; //Sets the Agent List invisable
+            agentloginbox.Visible = false; //Sets the Agent Login Box invisable
+            newagentbutton.Visible = false; //Sets the New Agent Button invisable
+            newagentlabel.Visible = false; //Sets the New Agent Button Label invisable
+            newagentcancelbutton.Visible = true; //Sets the New Agent Cancel button visable
+            newagentcancellabel.Visible = true; //Sets the New Agent Cancel Label visable
+            createagentbox.Visible = true; //Sets the Create Agent Box visable            
+        }
+
+        private void newagentcancelbutton_Click(object sender, EventArgs e)
+        {
+            agentlist.Visible = true; //Sets the Agent List visable
+            agentloginbox.Visible = true; //Sets the Agent Login Box visable
+            newagentbutton.Visible = true; //Sets the New Agent Button visable
+            newagentlabel.Visible = true; //Sets the New Agent Button Label visable
+            newagentcancelbutton.Visible = false; //Sets the New Agent Cancel button invisable
+            newagentcancellabel.Visible = false; //Sets the New Agent Cancel Label invisable
+            createagentbox.Visible = false; //Sets the Create Agent Box invisable
+            createusername.Text = "";
+            createpassword.Text = "";
+            createconfirmpassword.Text = "";
+        }
+
+        private void createagentbutton_Click(object sender, EventArgs e)
+        {
+            string causer;
+            string capass;
+            causer = createusername.Text; //Sets causer variable from creatusername textbox
+            if (createpassword.Text == createconfirmpassword.Text) //Checks to see if the passwords match
+            {
+                capass = createpassword.Text; //If passwords match then sets the variable from createpassword textbox
+                File.WriteAllText("agents/" + causer + ".hu", "Name:" + causer + "\r\n Password:" + capass + "\r\n Level:Newbie \r\n Money:5000c");
+            }
+            else //If the passwords do not match
+            {
+                createagenterror.Text = "Your password did not match."; //Sets createagenterror label
+            }
+        }
+
+        private void createusername_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
